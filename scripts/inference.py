d66af7ccac1146e37b3e774b2da04e7d0b8e968a
kavsar <kevserhuseynova@gmail.com>
2023-08-07 00:40:46 +0400

    treshold in terms of n_models

--- scripts/inference.py
+++ scripts/inference.py
@@ -1,6 +1,10 @@
 from models import DeepEnsemble
 
 load_path = "models/DE_v1"
-myde = DeepEnsemble.load_from_dir(load_path, no_models=3, threshold=0.66)
+
+no_models = 3
+threshold = int(round(no_models * 2 / 3) / no_models * 100) / 100
+myde = DeepEnsemble.load_from_dir(load_path, no_models=no_models, threshold=threshold)
+
 myde.quantize()
 print(myde.predict("gedek"))
